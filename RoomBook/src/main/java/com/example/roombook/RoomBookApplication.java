26e32cea48b61b349511191af10b5607f3af39e4
totszab <60985713+totszab@users.noreply.github.com>
2026-02-20 17:10:23 +0100

    Add files via upload

---
+++ RoomBook/src/main/java/com/example/roombook/RoomBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.roombook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomBookApplication.class, args);
+    }
+
+}
---
+++ RoomBook/src/test/java/com/example/roombook/RoomBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roombook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
