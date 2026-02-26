a3d5a8e7e711b00f4d9f76810c5ebf3a18ba6ba0
tothsz <szabolcs.toth@bragg.group>
2026-02-26 21:01:06 +0100

    use docker

--- RoomBook/src/main/java/com/example/roombook/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.roombook.repository;
-
-import com.example.roombook.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
